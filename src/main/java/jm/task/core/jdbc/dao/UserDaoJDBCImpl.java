c3368975d42b8e75564ee57fd14b78544c77ff48
iggoren <entaev@mail.ru>
2021-09-16 00:21:34 +0800

    Util

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 
 public class UserDaoJDBCImpl implements UserDao {
-    private static final Connection connection = Util.getConnection();
+    private final Connection connection = (Connection) Util.getConnection();
 
     public UserDaoJDBCImpl() {
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -9,24 +10,24 @@ public class Util {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
+    private Util() {
+    }
 
     public static Connection getConnection() {
         Connection connection = null;
 
         try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-//            Driver driver = new com.mysql.cj.jdbc.Driver();
-//            DriverManager.registerDriver(driver);
-
-            if (!connection.isClosed()) {
-                System.out.println("соединение установлено");
+            if (connection == null) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                if (!connection.isClosed()) {
+                    System.out.println("соединение установлено");
+                }
             }
-//
         } catch (
-                SQLException e) {
+                SQLException | ClassNotFoundException e) {
             System.out.println("ошибка соединения");
         }
         return connection;
     }
-
 }
